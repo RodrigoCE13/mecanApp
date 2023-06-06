8ba39d09d7e6cef559b36322f658fef6a027174b
RodrigoCE13 <rodrigo.carrasco@virginiogomez.cl>
2023-06-06 16:35:42 -0400

    se agrego el componente de valor vehiculo

---
+++ src/app/components/ver-valor/ver-valor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-valor',
+  templateUrl: './ver-valor.component.html',
+  styleUrls: ['./ver-valor.component.css']
+})
+export class VerValorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
